14a2165f18e47e51a3f9520b4b3af94a9bfe6b24
HaJunRyu <hjr4557@naver.com>
2023-02-11 01:45:55 +0900

    resize이벤트에 renderer의 크기 조정에 대한 대응, 디바이스 해상도 대응을 한다

---
+++ three-js-02/basic/src/ex02.js
@@ -0,0 +1,44 @@
+import * as THREE from 'three';
+
+// const renderer = new THREE.WebGLRenderer();
+// document.body.appendChild(renderer.domElement);
+
+export default function example2() {
+  const canvas = document.getElementById('three-canvas');
+  const renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  // canvas를 고해상도 디바이스에 대응 (image 고해상도 대응과 같은 맥락)
+  // 하지만 2배율을 초과하게 대응하면 성능상 손해가 크기 때문에 2배율로 많이쓴다고 함
+  renderer.setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1);
+
+  const scene = new THREE.Scene();
+
+  const ASPECT = window.innerWidth / window.innerHeight;
+
+  const camera = new THREE.PerspectiveCamera(75, ASPECT, 0.1, 1000);
+  camera.position.x = 1;
+  camera.position.y = 2;
+  camera.position.z = 5;
+  scene.add(camera);
+
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  const material = new THREE.MeshBasicMaterial({
+    color: 'red',
+  });
+
+  const mesh = new THREE.Mesh(geometry, material);
+  scene.add(mesh);
+
+  renderer.render(scene, camera);
+
+  function setSize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.render(scene, camera);
+  }
+
+  window.addEventListener('resize', setSize);
+}
