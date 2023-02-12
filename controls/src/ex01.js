9f7291d92c5f62be47002d43c1d191d0ad092356
HaJunRyu <hjr4557@naver.com>
2023-02-13 01:49:58 +0900

    OrbitControls 세부 설정을 적용해본다

---
+++ controls/src/ex01.js
@@ -0,0 +1,96 @@
+import * as THREE from 'three';
+import { Material } from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+// ----- 주제: OrbitControls
+
+export default function example() {
+  // Renderer
+  const canvas = document.querySelector('#three-canvas');
+  const renderer = new THREE.WebGLRenderer({
+    canvas,
+    antialias: true,
+  });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1);
+
+  // Scene
+  const scene = new THREE.Scene();
+
+  // Camera
+  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  camera.position.y = 1.5;
+  camera.position.z = 4;
+  scene.add(camera);
+
+  // Light
+  const ambientLight = new THREE.AmbientLight('white', 0.5);
+  scene.add(ambientLight);
+
+  const directionalLight = new THREE.DirectionalLight('white', 1);
+  directionalLight.position.x = 1;
+  directionalLight.position.z = 2;
+  scene.add(directionalLight);
+
+  // Controls
+  const controls = new OrbitControls(camera, renderer.domElement);
+  // x, y축 컨트롤을 더 부드럽게 처리
+  controls.enableDamping = true;
+  // zoom in, out disabled
+  // controls.enableZoom = false;
+  // z축 최대 조정 값을 설정
+  controls.maxDistance = 10;
+  // z축 최소 조정 값을 설정
+  controls.minDistance = 2;
+  // control할때 회전의 중심점을 설정
+  controls.target.set(2, 2, 2);
+  // 자동 회전
+  controls.autoRotate = true;
+  // 자동 회전 속도
+  controls.autoRotateSpeed = 10;
+
+  // Mesh
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  let mesh;
+  let material;
+  for (let i = 0; i < 20; i++) {
+    material = new THREE.MeshStandardMaterial({
+      color: `rgb(
+          ${Math.floor(Math.random() * 205) + 50},
+          ${Math.floor(Math.random() * 205) + 50},
+          ${Math.floor(Math.random() * 205) + 50}
+        )
+      `,
+    });
+    mesh = new THREE.Mesh(geometry, material);
+    mesh.position.x = (Math.random() - 0.5) * 5;
+    mesh.position.y = (Math.random() - 0.5) * 5;
+    mesh.position.z = (Math.random() - 0.5) * 5;
+
+    scene.add(mesh);
+  }
+
+  // 그리기
+  const clock = new THREE.Clock();
+
+  function draw() {
+    const delta = clock.getDelta();
+
+    controls.update();
+
+    renderer.render(scene, camera);
+    renderer.setAnimationLoop(draw);
+  }
+
+  function setSize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.render(scene, camera);
+  }
+
+  // 이벤트
+  window.addEventListener('resize', setSize);
+
+  draw();
+}
