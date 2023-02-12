da8231435c24df2dcf83cbfbacc3d67a2fed4e97
HaJunRyu <hjr4557@naver.com>
2023-02-12 23:16:58 +0900

    축, 그리드 헬퍼 예제 파일을 추가한다

---
+++ utility/src/ex01.js
@@ -0,0 +1,64 @@
+import * as THREE from 'three';
+
+// ----- 주제: AxesHelper, GridHelper
+
+export default function example() {
+	// Renderer
+	const canvas = document.querySelector('#three-canvas');
+	const renderer = new THREE.WebGLRenderer({
+		canvas,
+		antialias: true
+	});
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	renderer.setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1);
+
+	// Scene
+	const scene = new THREE.Scene();
+
+	// Camera
+	const camera = new THREE.PerspectiveCamera(
+		75,
+		window.innerWidth / window.innerHeight,
+		0.1,
+		1000
+	);
+	camera.position.z = 5;
+	scene.add(camera);
+
+	const light = new THREE.DirectionalLight(0xffffff, 1);
+	light.position.x = 1;
+	light.position.z = 2;
+	scene.add(light);
+
+	// Mesh
+	const geometry = new THREE.BoxGeometry(1, 1, 1);
+	const material = new THREE.MeshStandardMaterial({
+		color: 'seagreen'
+	});
+	const mesh = new THREE.Mesh(geometry, material);
+	scene.add(mesh);
+
+	// 그리기
+	const clock = new THREE.Clock();
+
+	function draw() {
+		const time = clock.getElapsedTime();
+
+		mesh.rotation.y = time;
+
+		renderer.render(scene, camera);
+		renderer.setAnimationLoop(draw);
+	}
+
+	function setSize() {
+		camera.aspect = window.innerWidth / window.innerHeight;
+		camera.updateProjectionMatrix();
+		renderer.setSize(window.innerWidth, window.innerHeight);
+		renderer.render(scene, camera);
+	}
+
+	// 이벤트
+	window.addEventListener('resize', setSize);
+
+	draw();
+}
