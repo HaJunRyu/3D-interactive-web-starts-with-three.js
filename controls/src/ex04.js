298ede24ad550d50be1c04d6d45e4a951d494858
HaJunRyu <hjr4557@naver.com>
2023-02-14 17:29:49 +0900

    FlyControls의 변형판인 FirstPersonControls를 적용한다

---
+++ controls/src/ex04.js
@@ -0,0 +1,89 @@
+import * as THREE from 'three';
+import { FirstPersonControls } from 'three/examples/jsm/controls/FirstPersonControls';
+
+// ----- 주제: OrbitControls
+
+export default function example() {
+  // Renderer
+  const canvas = document.querySelector('#three-canvas');
+  const renderer = new THREE.WebGLRenderer({
+    canvas,
+    antialias: true,
+  });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1);
+
+  // Scene
+  const scene = new THREE.Scene();
+
+  // Camera
+  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  camera.position.y = 1.5;
+  camera.position.z = 4;
+  scene.add(camera);
+
+  // Light
+  const ambientLight = new THREE.AmbientLight('white', 0.5);
+  scene.add(ambientLight);
+
+  const directionalLight = new THREE.DirectionalLight('white', 1);
+  directionalLight.position.x = 1;
+  directionalLight.position.z = 2;
+  scene.add(directionalLight);
+
+  // Controls
+  const controls = new FirstPersonControls(camera, renderer.domElement);
+  // 앞, 뒤, 좌, 우로 움직이는 속도를 조정
+  controls.movementSpeed = 10;
+  // 마우스를 이용한 rotate 가능 여부 설정
+  // controls.activeLook = false;
+  // 마우스 커서 방향으로 rotate되는 속도를 조정
+  controls.lookSpeed = 0.1;
+  // 자동으로 앞으로 이동하게끔 설정
+  controls.autoForward = true;
+
+  // Mesh
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  let mesh;
+  let material;
+  for (let i = 0; i < 20; i++) {
+    material = new THREE.MeshStandardMaterial({
+      color: `rgb(
+          ${Math.floor(Math.random() * 205) + 50},
+          ${Math.floor(Math.random() * 205) + 50},
+          ${Math.floor(Math.random() * 205) + 50}
+        )
+      `,
+    });
+    mesh = new THREE.Mesh(geometry, material);
+    mesh.position.x = (Math.random() - 0.5) * 5;
+    mesh.position.y = (Math.random() - 0.5) * 5;
+    mesh.position.z = (Math.random() - 0.5) * 5;
+
+    scene.add(mesh);
+  }
+
+  // 그리기
+  const clock = new THREE.Clock();
+
+  function draw() {
+    const delta = clock.getDelta();
+
+    controls.update(delta);
+
+    renderer.render(scene, camera);
+    renderer.setAnimationLoop(draw);
+  }
+
+  function setSize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.render(scene, camera);
+  }
+
+  // 이벤트
+  window.addEventListener('resize', setSize);
+
+  draw();
+}
