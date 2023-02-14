87183a0a16441ad4f80a81dde89ce8cc5dccf378
HaJunRyu <hjr4557@naver.com>
2023-02-14 17:16:33 +0900

    TrackballControl을 적용한다

---
+++ controls/src/ex02.js
@@ -0,0 +1,91 @@
+import * as THREE from 'three';
+import { TrackballControls } from 'three/examples/jsm/controls/TrackballControls';
+
+// ----- 주제: OrbitControls
+
+export default function example() {
+  // Renderer
+  const canvas = document.querySelector('#three-canvas');
+  const renderer = new THREE.WebGLRenderer({
+    canvas,
+    antialias: true,
+  });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1);
+
+  // Scene
+  const scene = new THREE.Scene();
+
+  // Camera
+  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  camera.position.y = 1.5;
+  camera.position.z = 4;
+  scene.add(camera);
+
+  // Light
+  const ambientLight = new THREE.AmbientLight('white', 0.5);
+  scene.add(ambientLight);
+
+  const directionalLight = new THREE.DirectionalLight('white', 1);
+  directionalLight.position.x = 1;
+  directionalLight.position.z = 2;
+  scene.add(directionalLight);
+
+  // Controls
+  const controls = new TrackballControls(camera, renderer.domElement);
+  // x, y축 컨트롤을 더 부드럽게 처리, TrackballControl을 이 설정이 기본으로 적용되어 있다.
+  // controls.enableDamping = true;
+  // zoom in, out disabled
+  // controls.enableZoom = false;
+  // z축 최대 조정 값을 설정
+  controls.maxDistance = 20;
+  // z축 최소 조정 값을 설정
+  controls.minDistance = 5;
+  // control할때 회전의 중심점을 설정
+  controls.target.set(2, 2, 2);
+
+  // Mesh
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  let mesh;
+  let material;
+  for (let i = 0; i < 20; i++) {
+    material = new THREE.MeshStandardMaterial({
+      color: `rgb(
+          ${Math.floor(Math.random() * 205) + 50},
+          ${Math.floor(Math.random() * 205) + 50},
+          ${Math.floor(Math.random() * 205) + 50}
+        )
+      `,
+    });
+    mesh = new THREE.Mesh(geometry, material);
+    mesh.position.x = (Math.random() - 0.5) * 5;
+    mesh.position.y = (Math.random() - 0.5) * 5;
+    mesh.position.z = (Math.random() - 0.5) * 5;
+
+    scene.add(mesh);
+  }
+
+  // 그리기
+  const clock = new THREE.Clock();
+
+  function draw() {
+    const delta = clock.getDelta();
+
+    controls.update();
+
+    renderer.render(scene, camera);
+    renderer.setAnimationLoop(draw);
+  }
+
+  function setSize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.render(scene, camera);
+  }
+
+  // 이벤트
+  window.addEventListener('resize', setSize);
+
+  draw();
+}
