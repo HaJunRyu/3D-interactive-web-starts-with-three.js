693fd5a9c9516e3893956bd0dc5d5ea96ed132e5
HaJunRyu <hjr4557@naver.com>
2023-02-15 23:22:45 +0900

    간단한 texture 변환 속성을 적용해본다

---
+++ material/src/ex07.js
@@ -0,0 +1,105 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+// ----- 주제: Texture Image 로드하기
+
+export default function example() {
+  const loadingManager = new THREE.LoadingManager();
+  loadingManager.onStart = () => {
+    console.log('로드 시작');
+  };
+  loadingManager.onProgress = img => {
+    console.log(img + ' 로드');
+  };
+  loadingManager.onLoad = () => {
+    console.log('로드 완료');
+  };
+  loadingManager.onError = () => {
+    console.log('에러');
+  };
+
+  // 텍스쳐 이미지 로드
+  const textureLoader = new THREE.TextureLoader(loadingManager);
+
+  const texture = textureLoader.load('./textures/skull/Ground Skull_basecolor.jpg');
+
+  // 텍스쳐 변환
+  texture.wrapS = THREE.RepeatWrapping;
+  texture.wrapT = THREE.RepeatWrapping;
+
+  // texture.offset.x = 0.3;
+  // texture.offset.y = 0.3;
+
+  // texture.repeat.x = 2;
+  // texture.repeat.y = 2;
+
+  texture.rotation = Math.PI * 0.25;
+  texture.rotation = THREE.MathUtils.degToRad(0);
+  texture.center.x = 0.5;
+  texture.center.y = 0.5;
+
+  // Renderer
+  const canvas = document.querySelector('#three-canvas');
+  const renderer = new THREE.WebGLRenderer({
+    canvas,
+    antialias: true,
+  });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1);
+
+  // Scene
+  const scene = new THREE.Scene();
+  scene.background = new THREE.Color('white');
+
+  // Camera
+  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  camera.position.y = 1.5;
+  camera.position.z = 4;
+  scene.add(camera);
+
+  // Light
+  const ambientLight = new THREE.AmbientLight('white', 0.5);
+  scene.add(ambientLight);
+
+  const directionalLight = new THREE.DirectionalLight('white', 1);
+  directionalLight.position.x = 1;
+  directionalLight.position.z = 2;
+  scene.add(directionalLight);
+
+  // Controls
+  const controls = new OrbitControls(camera, renderer.domElement);
+
+  // Mesh
+  const geometry = new THREE.BoxGeometry(2, 2, 2);
+  // 하이라이트, 반사광 표현 가능한 재질, 성능은 Phong이 Standard보다 좋다고함
+  const material = new THREE.MeshStandardMaterial({
+    map: texture,
+  });
+  // 하이라이트, 반사광이 표현 가능한 재질
+
+  const mesh = new THREE.Mesh(geometry, material);
+
+  scene.add(mesh);
+
+  // 그리기
+  const clock = new THREE.Clock();
+
+  function draw() {
+    const delta = clock.getDelta();
+
+    renderer.render(scene, camera);
+    renderer.setAnimationLoop(draw);
+  }
+
+  function setSize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.render(scene, camera);
+  }
+
+  // 이벤트
+  window.addEventListener('resize', setSize);
+
+  draw();
+}
